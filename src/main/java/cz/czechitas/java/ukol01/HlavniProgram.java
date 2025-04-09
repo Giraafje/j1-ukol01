467e032cb1f952c219978fe5d937c06e606c7b9f
Lenka Erbenova <lenka.erbenova79@gmail.com>
2025-04-09 21:18:00 +0200

    Turtle Zofka draws a pig.

--- src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -11,8 +11,53 @@ public class HlavniProgram {
 
   public void start() {
     zofka = new Turtle();
+    zofka.setPenWidth(5);
+    drawPig();
+  }
 
-    //TODO implementace domácího úkolu
+  public void drawHouse() {
+    for (int i = 0; i < 4; i++) {
+      zofka.move(50);
+      zofka.turnRight(90);
+    }
+    zofka.move(50);
+    zofka.turnRight(45);
+    zofka.move(35);
+    zofka.turnRight(90);
+    zofka.move(35);
+    zofka.turnRight(45);
+    zofka.move(50);
+    zofka.turnRight(180);
+  }
+
+  public void drawLeg() {
+    zofka.move(20);
+    zofka.turnRight(180);
+    zofka.move(20);
+  }
+
+  public void drawPig() {
+    zofka.turnLeft(90);
+    drawHouse();
+    zofka.turnLeft(90);
+    zofka.move(50);
+
+    // legs on the left side
+    zofka.turnLeft(45);
+    drawLeg();
+    zofka.turnLeft(90);
+    drawLeg();
+
+    zofka.turnLeft(135);
+    zofka.move(50);
+
+    // legs on the right side
+    zofka.turnLeft(45);
+    drawLeg();
+    zofka.turnRight(90);
+    drawLeg();
+
+    zofka.turnLeft(45);
   }
 
 }
